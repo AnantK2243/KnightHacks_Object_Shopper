505ba738249557ab4294835aed0b7bbf42011d12
Anant Kumar <anantone72@gmail.com>
2023-10-07 17:55:43 -0400

    Commit 10

--- webscraper.py
+++ webscraper.py
@@ -1,38 +1,30 @@
-from googlesearch import search
-import pandas as pd
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
-
-
-# Step 1. Getting top n urls from Google's search results
-
-def get_links_from_google(term, num_results=10, lang='en'):
-    url_list = [x for x in search(term=term, lang=lang, num_results=num_results)]
-    return pd.DataFrame(url_list, columns=['url'])
-
-
-# Testing step 1
-df = get_links_from_google('Tv', 5)
-
 
 # Step 2. Getting the content of the top n pages
 
-def get_page_content(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+def get_page_content(item):
+    l = []
+    o = []
+
+    targetURL = 'https://www.amazon.com/s?k=' + item
+    
+    headers1 = {
+        "accept-language": "en-US,en;q=0.9","accept-encoding": "gzip, deflate, br",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0',
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
     }
 
-    with HTMLSession() as session:
-        try:
-            res = session.get(url, headers=headers, timeout=200)
-            return BeautifulSoup(res.content, 'html.parser').text
-        except:
-            return BeautifulSoup('', 'html.parser').text
-        finally:
-            print(f"Done Scrapping")
+    resp = requests.get(targetURL, headers=headers1)
+    
+    soup = BeautifulSoup(resp.text,'html.parser')
 
-# Testing step 2
-df['text'] = df['url'].apply(lambda x: get_page_content(x))
-df.to_pickle('data.pkl')
+    try:
+        o.append(soup.find_all("span",{"class":"a-size-medium a-color-base a-text-normal"}))
+        #o.append(soup.find_all("span",{"class":"a-price"}).find("span").text)
+    except:
+        o.append(None)
 
-print(df)
+    print(o)
+
+item = get_page_content("TV")
