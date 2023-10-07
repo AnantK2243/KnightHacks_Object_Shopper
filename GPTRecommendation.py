8757d05b48fab7b2623cd8f356dd60ddd8bad951
Anant Kumar <anantone72@gmail.com>
2023-10-07 16:36:03 -0400

    Commit 8

--- GPTRecommendation.py
+++ GPTRecommendation.py
@@ -6,17 +6,16 @@ with open('config.json', 'r') as config_file:
     config_data = json.load(config_file)
 api_key = config_data.get("api_key")
 
-
 # Inititate openAi
 openai.api_key = api_key
 
 # Get recommendation form ChatGPT 3.5
 def get_Recommendation(prompt):
-    prompt1 = "Give me a shopping recommendation that pairs well with a " + prompt
+    prompt1 = "Give me 3 shopping recommendation that pairs well with a " + prompt + " without explanation or the word a seperated by commas so I can split using python code"
     response = openai.Completion.create(
         engine="text-davinci-002", 
         prompt=prompt1,
-        max_tokens=300,
+        max_tokens=100,
         n=3,
         stop=None,
     )
@@ -24,7 +23,6 @@ def get_Recommendation(prompt):
 
 def main():
     print("In Main")
-
 
 if __name__ == "__main__":
     main()
