8f7a1d07ed8c9912365c7216ed1f9ca055920d40
Anant Kumar <anantone72@gmail.com>
2023-10-07 02:46:14 -0400

    Commit 5

--- GPTRecommendation.py
+++ GPTRecommendation.py
@@ -1,4 +1,4 @@
-# import openai
+import openai
 import json
 
 # Get API KEY
@@ -6,21 +6,24 @@ with open('config.json', 'r') as config_file:
     config_data = json.load(config_file)
 api_key = config_data.get("api_key")
 
-print(f"{api_key}")
 
 # Inititate openAi
-# openai.api_key = api_key
+openai.api_key = api_key
 
-
-"""
 # Get recommendation form ChatGPT 3.5
 def get_Reccomendation(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002", 
-        prompt=prompt,
-        max_tokens=50,  # Adjust as needed
-        n=1,  # Number of completions to generate
-        stop=None,  # Stop generation at specified tokens (e.g., ['\n'])
+        prompt= ("Give me a similar item like " + prompt),
+        max_tokens=10,
+        n=3,
+        stop=None,
     )
     return response.choices[0].text.strip()
-"""
+
+def main():
+    print("In Main")
+
+
+if __name__ == "__main__":
+    main()
